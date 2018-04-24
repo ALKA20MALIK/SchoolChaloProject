38f8b3f1110d7e56f1d13ca4ea087090d1647cb0
ALKAMALIK <malikalka2090>
2018-04-24 20:49:29 +0530

    hi

---
+++ SchoolChalo/Controllers/CandidateLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolChalo.Controllers
+{
+    public class CandidateLoginController : Controller
+    {
+        // GET: Login
+        public string CandidateLogin()
+        {
+            return "Hello Candidate";
+        }
+    }
+}
--- SchoolChalo/Controllers/HomeController.cs
+++ SchoolChalo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SchoolChalo.Models;
 
 namespace SchoolChalo.Controllers
 {
@@ -25,6 +26,28 @@ namespace SchoolChalo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(xtRegistration obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                LOCALEntities db = new LOCALEntities();
+                db.xtRegistrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View("Login");
         }
     }
 }
