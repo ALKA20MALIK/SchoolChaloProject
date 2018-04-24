aa035bbe11d15cd10808b42812824b7fa66b5f5c
ALKAMALIK <malikalka2090>
2018-04-24 21:35:23 +0530

    hi

--- SchoolChalo/Controllers/CandidateLoginController.cs
+++ SchoolChalo/Controllers/CandidateLoginController.cs
@@ -11,7 +11,7 @@ namespace SchoolChalo.Controllers
         // GET: Login
         public string CandidateLogin()
         {
-            return "Hello 1Aslka2 Candidate";
+            return "Hello alka Candidate";
         }
     }
 }
